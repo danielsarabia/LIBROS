8aac3accd69054af257f7623a6de498bedd63f89
Armando93 <argarch@hotmail.com>
2013-11-17 14:15:54 -0600

    Ahora con las caracteristicas
    ahorita le doy formato a eso

--- app/templates/mostrarLibros.php
+++ app/templates/mostrarLibros.php
@@ -1,7 +1,7 @@
 <?php ob_start() ?>
 <?php foreach ($params['libros'] as $dato) :?>
 <div class="thumbnail">
-<img src="img/<?php echo $dato['foto'] ?>" width="100" style="float:left;"> <p href="index.php?ctl=ver&id=<?php echo $dato['id']?>"><?php echo $dato['titulo'] ?></p><br /><p>$<?php echo $dato['precio'] ?></p>
+<img src="img/<?php echo $dato['foto'] ?>" width="100" style="float:left;"> <a href="index.php?ctl=ver&id=<?php echo $dato['id']?>"><?php echo $dato['titulo'] ?></a><br /><p>$<?php echo $dato['precio'] ?></p>
 </div>
 <?php endforeach; ?>
  
