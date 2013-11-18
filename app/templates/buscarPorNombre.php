2d760f909c42b73704d56a2584aa84cb1ebd5a05
Armando93 <argarch@hotmail.com>
2013-11-17 21:03:24 -0600

    Ver detalles en busqueda

--- app/templates/buscarPorNombre.php
+++ app/templates/buscarPorNombre.php
@@ -1,9 +1,9 @@
 <?php ob_start() ?>
-
+<h1>RESULTADOS DE LA BUSQUEDA</h1>
       <?php if (count($params['resultado'])>0): ?>
       <?php foreach ($params['resultado'] as $dato) :?>
 <div class="thumbnail">
-<img src="img/<?php echo $dato['foto'] ?>" width="100" style="float:left;"> <p><?php echo $dato['titulo'] ?></p><br /><p>$<?php echo $dato['precio'] ?></p>
+<img src="img/<?php echo $dato['foto'] ?>" width="100" style="float:left;"> <a href="index.php?ctl=ver&id=<?php echo $dato['id']?>"><?php echo $dato['titulo'] ?></p><br /><p>$<?php echo $dato['precio'] ?></p>
 </div>
 <?php endforeach; ?>
       
